d940b82f6b420ff85cdb11e20ebc7f1b5ce0ac4e
Amin Shah Gilani <amin@gilani.me>
2020-12-26 18:03:34 -0500

    Add a waitlist app with a waitlist model

---
+++ waitlist/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+class WaitList(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
