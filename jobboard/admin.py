e1083563921601744c8997afb0dc659ac1d817fe
Amin Shah Gilani <amin@gilani.me>
2020-12-26 21:33:47 -0500

    Add Employers and update old migration to match

--- jobboard/admin.py
+++ jobboard/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from .models import JobListing
+from .models import Employer
 
 admin.site.register(JobListing)
+admin.site.register(Employer)
--- jobboard/models.py
+++ jobboard/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 
-class JobListing(models.Model):
-    title = models.CharField(max_length=254)
-    description = models.TextField()
+class Employer(models.Model):
+    """The employers that employees"""
+
+    name = models.CharField(max_length=254)
 
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.name}"
+
+
+class JobListing(models.Model):
+    """The listings that contain individual job specifications"""
+
+    title = models.CharField(max_length=254)
+    description = models.TextField()
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} @ {self.employer.name}"
