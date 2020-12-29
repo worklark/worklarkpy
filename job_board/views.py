4078cd036e9efe8a1a719ece66dbea5d5f17ff1d
Amin Shah Gilani <amin@gilani.me>
2020-12-29 07:06:07 -0500

    Add template inheritance

--- job_board/views.py
+++ job_board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.forms import modelform_factory
 
 from .models import JobListing, Employer
@@ -13,7 +13,13 @@ JobListingForm = modelform_factory(JobListing, exclude=[])
 
 
 def job_listing_new(request):
-    form = JobListingForm
+    if request.method == "POST":
+        form = JobListingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = JobListingForm
     return render(request, "job_board/job_listing_new.html", {"form": form})
 
 
@@ -29,14 +35,20 @@ def employer(request, id):
     return render(request, "job_board/employer.html", {"employer": employer})
 
 
-def employers(request):
-    employers = get_list_or_404(Employer)
-    return render(request, "job_board/employers.html", {"employers": employers})
-
-
 EmployerForm = modelform_factory(Employer, exclude=[])
 
 
 def employer_new(request):
-    form = EmployerForm
+    if request.method == "POST":
+        form = EmployerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = EmployerForm
     return render(request, "job_board/employer_new.html", {"form": form})
+
+
+def employers(request):
+    employers = get_list_or_404(Employer)
+    return render(request, "job_board/employers.html", {"employers": employers})
